31ca420818edbb2d6c524c55bd26f0e0041f75ef
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-04-14 19:23:24 +0900

    (2023.4.14) 깃허브 데스크탑 연동

---
+++ Game/Assets/Tutorial Script/B.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B : MonoBehaviour
+{
+    public A aClass;
+
+    void Start()
+    {
+        aClass.data = 300;
+    }
+
+    void Update()
+    {
+        
+    }
+}
