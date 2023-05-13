454c4323967395e56b23555c6e1c28e64ef84400
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-05-13 17:17:55 +0900

    애니메이션 작업 추가

---
+++ Animation/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Vector3 direction = new Vector3(1,1,1);
+
+    void Start()
+    {
+        print(transform.position); // 0,0,0
+        Move(transform);
+        print(transform.position); // 5,5,5
+    }
+
+    public void Move(Transform dir)
+    {
+        dir.position = new Vector3(5, 5, 5);
+    }
+
+
+}
