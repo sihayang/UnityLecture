dfe6e7ddee31be08801096d22997fc0f502f6cde
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-06-03 17:36:03 +0900

    (22023.6.3) 물리 시스템 사용

---
+++ Animation/Assets/Physics/Script/BilliardBall.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class BilliardBall : MonoBehaviour
+{
+    private Vector3 direction;
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] Rigidbody rigidBody;
+
+    private void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.AddForce
+        (
+            direction * speed,
+            ForceMode.Acceleration // 연속적인 힘 (무게 적용)
+        );
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Pillar"))
+        {
+            var result = Vector3.Reflect
+            (
+                transform.position.normalized,
+                collision.contacts[0].normal
+            );
+
+            rigidBody.velocity = result * Mathf.Max(speed, 0);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Billiard Ball"))
+        {
+            rigidBody.AddTorque
+            (
+                Vector3.up * speed,
+                ForceMode.Impulse // 순간적인 힘 (무게 적용)
+            );
+        }
+    }
+    
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Pillar"))
+        {
+            int randomMode = Random.Range(0, 3);
+            rigidBody.interpolation = (RigidbodyInterpolation)randomMode;
+        }
+    }
+}
